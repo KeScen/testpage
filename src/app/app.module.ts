693601850c493034d729ae507d631b06d9357770
kescen <scapatik@gmail.com>
2019-06-06 18:38:43 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClassesComponent } from './classes/classes.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTooltipModule} from '@angular/material/tooltip';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClassesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTooltipModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
